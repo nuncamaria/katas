b4c9ca7e892117a15ce7e944156a0b7f837be1df
maria.nunez <maria.nunez@wefox.com>
2022-07-06 16:37:45 +0200

    add kata info

---
+++ app/src/main/java/com/marianunez/katas/03NumeroPrimo.kt
@@ -0,0 +1,10 @@
+package com.marianunez.katas
+
+/*
+ * Challenge #3
+ * ¿ES UN NÚMERO PRIMO?
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
