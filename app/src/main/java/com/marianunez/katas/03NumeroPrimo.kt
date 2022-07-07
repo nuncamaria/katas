32ed1bea9332f868a107983ef9e6b3e7c58bc063
maria.nunez <maria.nunez@wefox.com>
2022-07-07 18:16:34 +0200

    validation in progress

--- app/src/main/java/com/marianunez/katas/03NumeroPrimo.kt
+++ app/src/main/java/com/marianunez/katas/03NumeroPrimo.kt
@@ -1,5 +1,7 @@
 package com.marianunez.katas
 
+import kotlin.random.Random
+
 /*
  * Challenge #3
  * ¿ES UN NÚMERO PRIMO?
@@ -7,4 +9,31 @@ package com.marianunez.katas
  *
  * Enunciado: Escribe un programa que se encargue de comprobar si un número es o no primo.
  * Hecho esto, imprime los números primos entre 1 y 100.
+ *
+ * Si es 2 es primo
+ * Si no es par excepto si es 2, no es primo
+ *
  */
+
+
+private fun main() {
+    calcPrimeNumber()
+    isPrime()
+}
+
+private fun calcPrimeNumber() {
+    val listOfNumbers = listOf(0..100)
+
+    listOfNumbers.forEach {
+        println(it)
+    }
+}
+
+
+private fun isPrime(number: Int = Random.nextInt(0, 100)) {
+    if ((number > 2) && (number % 2 == 0)) {
+        println("$number no es primo SEGURO")
+    } else {
+        println("$number quizás es primo")
+    }
+}
