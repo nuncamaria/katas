4839bca7f51c5f4c2444c495d2570f4f83caaf1f
maria.nunez <maria.nunez@wefox.com>
2022-07-10 14:31:19 +0200

    finished

--- app/src/main/java/com/marianunez/katas/03NumeroPrimo.kt
+++ app/src/main/java/com/marianunez/katas/03NumeroPrimo.kt
@@ -17,27 +17,35 @@ import kotlin.random.Random
 
 const val FIRST_NUMBER = 1
 const val LAST_NUMBER = 100
+var counter = 0
 
-private fun main() = isPrime()
-
-private fun isPrime(input: Int = Random.nextInt(FIRST_NUMBER, LAST_NUMBER)) {
-    val listOfNumbers = FIRST_NUMBER..input
-    listOfNumbers.map { number ->
-        calc(number, listOfNumbers)
-    }
+private fun main() {
+    renderPrime(Random.nextInt(FIRST_NUMBER, LAST_NUMBER))
 }
 
-fun calc(a: Int, b: IntRange) {
-    var counter = 0
-    b.map {
-        if (a % it == 0) {
-            counter++
-        }
-    }
+private fun renderPrime(input: Int) {
+    val rangeOfNumbers = FIRST_NUMBER..input
+    val totalOfNumbers = FIRST_NUMBER..LAST_NUMBER
+
+    println("1ª part: Check if a number is prime or not")
+    countPrimes(input, rangeOfNumbers)
+    println("-")
+    println("2ª part: Print prime numbers from 1 to 100")
+    totalOfNumbers.forEach { countAllPrimes(it, totalOfNumbers) }
+}
+
+private fun countPrimes(number: Int, rangeOfNumbers: IntRange) {
+    counter = 0 // el counter checkea las veces que, dividiendo el número, su resto da 0
+    rangeOfNumbers.forEach { if (number % it == 0) counter++ }
 
     if (counter == 2) {
-        println("El número $a es primo")
-    } else {
-        println("El número $a no es primo")
-    }
+        println("****** Is prime: $number")
+    } else println("Number $number is not prime")
+}
+
+private fun countAllPrimes(number: Int, rangeOfNumbers: IntRange) {
+    counter = 0
+    rangeOfNumbers.forEach { if (number % it == 0) counter++ }
+
+    if (counter == 2) print("$number ")
 }
