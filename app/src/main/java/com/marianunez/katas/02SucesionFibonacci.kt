86cab62b846744825a18a8b8e6b53e6490f23f4e
maria.nunez <maria.nunez@wefox.com>
2022-07-10 20:11:34 +0200

    write planning

--- app/src/main/java/com/marianunez/katas/02SucesionFibonacci.kt
+++ app/src/main/java/com/marianunez/katas/02SucesionFibonacci.kt
@@ -9,3 +9,12 @@ package com.marianunez.katas
  * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
  * 0, 1, 1, 2, 3, 5, 8, 13...
  */
+
+private fun main() {
+
+}
+
+// hacer una lista vacía
+// meter los dos primeros números que serán 0 y 1
+// con eso ya podemos plantear la operación de que los dos últimos se sumen
+// que la lista se vaya llenando con esos resultados hasta que llegue a 50
