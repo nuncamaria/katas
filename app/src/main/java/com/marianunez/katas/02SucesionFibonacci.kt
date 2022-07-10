2b1b331ce6ba48eb4e84bb6887502e16a3140b27
maria.nunez <maria.nunez@wefox.com>
2022-07-10 21:05:11 +0200

    finished

--- app/src/main/java/com/marianunez/katas/02SucesionFibonacci.kt
+++ app/src/main/java/com/marianunez/katas/02SucesionFibonacci.kt
@@ -11,10 +11,24 @@ package com.marianunez.katas
  */
 
 private fun main() {
-
+    renderFibonacci()
 }
 
-// hacer una lista vacía
-// meter los dos primeros números que serán 0 y 1
+// create empty list
+// add the first two numbers: 0 y 1
 // con eso ya podemos plantear la operación de que los dos últimos se sumen
 // que la lista se vaya llenando con esos resultados hasta que llegue a 50
+
+private fun renderFibonacci() {
+    val fibonacciNumbers = arrayListOf(0)
+    fibonacciNumbers.add(1)
+    var position = 0
+
+    while (fibonacciNumbers.size <= 50) {
+        position++
+        val fibonacciNumber = fibonacciNumbers.last() + fibonacciNumbers[position - 1]
+        fibonacciNumbers.add(fibonacciNumber)
+    }
+
+    println(fibonacciNumbers)
+}
