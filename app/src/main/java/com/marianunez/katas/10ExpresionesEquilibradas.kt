97ef6383e5dc5f11d477be27c5e05869d8925761
maria.nunez <maria.nunez@wefox.com>
2022-08-08 16:42:42 +0200

    starting

---
+++ app/src/main/java/com/marianunez/katas/10ExpresionesEquilibradas.kt
@@ -0,0 +1,23 @@
+package com.marianunez.katas
+
+/*
+ * Challenge #10
+ * EXPRESIONES EQUILIBRADAS
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Crea un programa que comprueba si los paréntesis, llaves y corchetes de una expresión están equilibrados.
+ * - Equilibrado significa que estos delimitadores se abren y cierran en orden y de forma correcta.
+ * - Paréntesis, llaves y corchetes son igual de prioritarios. No hay uno más importante que otro.
+ * - Expresión balanceada: { [ a * ( c + d ) ] - 5 }
+ * - Expresión no balanceada: { a * ( c + d ) ] - 5 }
+ */
+
+fun main() {
+    isBalanced("{ [ a * ( c + d ) ] - 5 }")
+}
+
+fun isBalanced(expression: String){
+
+    val regex = "{}[]()".toRegex()
+
+}
